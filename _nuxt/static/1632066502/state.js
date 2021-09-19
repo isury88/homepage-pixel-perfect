eb834e8067293ccf39906d7f569f29aef1313565
isury88 <isury@hotmail.it>
2021-09-19 16:57:37 +0100

    0a7a624

---
+++ _nuxt/static/1632066502/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhomepage-pixel-perfect\u002F_nuxt\u002Fstatic\u002F1632066502",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhomepage-pixel-perfect\u002F",assetsPath:"\u002Fhomepage-pixel-perfect\u002F_nuxt\u002F",cdnURL:a}}}}(null));
